524042139f7aabcf7d9326d165b51a54eaa77aee
Amit Paudel <amitpaudel789@gmail.com>
2026-07-11 23:19:26 +0545

    Comment feature added

--- routes/blog.js
+++ routes/blog.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const multer = require('multer');
 const path = require('path');
 const Blog = require('../models/blog');
+const { Comment } = require('../models/comment');
 
 const router = Router();
 
@@ -23,6 +24,29 @@ router.get('/add-new', (req, res, next) => {
   });
 });
 
+router.get('/:id', async (req, res) => {
+  const blog = await Blog.findById(req.params.id).populate('createdBy');
+  const comments = await Comment.find({ blogId: req.params.id }).populate(
+    'createdBy',
+  );
+  console.log(blog);
+  console.log(comments);
+  return res.render('blog', {
+    user: req.user,
+    blog,
+    comments,
+  });
+});
+
+router.post('/comment/:blogId', async (req, res) => {
+  await Comment.create({
+    content: req.body.content,
+    blogId: req.params.blogId,
+    createdBy: req.user._id,
+  });
+  return res.redirect(`/blog/${req.params.blogId}`);
+});
+
 router.post('/', upload.single('coverImage'), async (req, res) => {
   const { title, body } = req.body;
   console.log(req.body);
@@ -30,7 +54,7 @@ router.post('/', upload.single('coverImage'), async (req, res) => {
     title,
     body,
     createdBy: req.user._id,
-    coverImageURL: `uploads/${req.file.filename}`,
+    coverImageURL: `/uploads/${req.file.filename}`,
   });
   return res.redirect(`/blog/${blog._id}`);
 });
